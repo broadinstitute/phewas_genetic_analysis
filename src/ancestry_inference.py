765851044cdaa22d082efa0a6472e0a9c0556b47
seuchoi <seuchoi@gmail.com>
2022-01-20 16:19:52 -0500

    update as a function

--- src/ancestry_inference.py
+++ src/ancestry_inference.py
@@ -1,11 +1,45 @@
+#!/usr/bin/python
+
+import sys, getopt
+
+def main(argv):
+   inputfile = ''
+   loadingfile = ''
+   RFmodelfile = ''
+   outputfile = ''
+   try:
+      opts, args = getopt.getopt(argv,"hi:l:m:o",["ifile=","lfile=","mfile=","ofile="])
+   except getopt.GetoptError:
+      print 'test.py -i <inputfile> -l <loadingfile> -m <RFmodelfile> -o <outputfile>'
+      sys.exit(4)
+   for opt, arg in opts:
+      if opt == '-h':
+         print 'ancestry_inference.py -i <inputfile> -l <loadingfile> -m <RFmodelfile> -o <outputfile>'
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+      elif opt in ("-l", "--lfile"):
+         loadingfile = arg
+      elif opt in ("-m", "--mfile"):
+         RFmodelfile = arg
+      elif opt in ("-o", "--ofile"):
+         outputfile = arg
+   print 'Input file is "', inputfile
+   print 'Loadings file is "', outputfile
+   print 'Random forest model file is "', inputfile
+   print 'Output file is "', outputfile
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
 import pickle
 import hail as hl
 from gnomad.sample_qc.ancestry import assign_population_pcs
 hl.init()
 
 # Load MatrixTable for projection and gnomAD loadings Hail Table
-mt_to_project = hl.read_matrix_table("gs://fc-secure-4adb21f6-46f4-4a79-99f9-afd63890c6d0/data/AoU_22K_WGS_all.mt")
-loadings_ht = hl.read_table("gs://gcp-public-data--gnomad/release/3.1/pca/gnomad.v3.1.pca_loadings.ht")
+mt_to_project = hl.read_matrix_table(inputfile)
+loadings_ht = hl.read_table(loadingfile)
 
 # Project new genotypes onto loadings
 ht = hl.experimental.pc_project(
@@ -16,7 +50,7 @@ ht = hl.experimental.pc_project(
 
 # Assign global ancestry using the gnomAD RF model and PC project scores
 # Loading of the v2 RF model requires an older version of scikit-learn, this can be installed using pip install -U scikit-learn==0.21.3
-with hl.hadoop_open("gs://gcp-public-data--gnomad/release/3.1/pca/gnomad.v3.1.RF_fit.pkl", "rb") as f:
+with hl.hadoop_open(RFmodelfile, "rb") as f:
     fit = pickle.load(f)
 
 # Reduce the scores to only those used in the RF model, this was 6 for v2 and 16 for v3.1
@@ -28,4 +62,4 @@ ht, rf_model = assign_population_pcs(
     fit=fit,
 )
 ht.show(5)
-ht.export("gs://fc-secure-a7a6da68-f796-46ad-86bb-4b56f01bfeb2/data/gnomad_inferred.tsv.bgz", delimiter='\t')
+ht.export(outputfile, delimiter='\t')
