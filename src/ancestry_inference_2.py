d56e0863e9dbcf5c9b054c4033b1dfc89408d86a
seuchoi <seuchoi@gmail.com>
2022-01-21 11:51:52 -0500

    test

--- src/ancestry_inference_2.py
+++ src/ancestry_inference_2.py
@@ -59,7 +59,7 @@ param_dict = parse_param()
 
 # Assign global ancestry using the gnomAD RF model and PC project scores
 # Loading of the v2 RF model requires an older version of scikit-learn, this can be installed using pip install -U scikit-learn==0.21.3
-with hl.hadoop_open("gs://gcp-public-data--gnomad/release/3.1/pca/gnomad.v3.1.RF_fit.pkl", "rb") as f:
+with hl.hadoop_open(param_dict['RFmodelfile'], "rb") as f:
         fit = pickle.load(f)
 
 # Load MatrixTable for projection and gnomAD loadings Hail Table
