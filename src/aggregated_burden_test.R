a67922ee73c81f9dcf627d6444ea1f20110d74a6
seuchoi <seuchoi@gmail.com>
2022-03-24 22:25:40 -0400

    add a new variable

--- src/aggregated_burden_test.R
+++ src/aggregated_burden_test.R
@@ -11,10 +11,11 @@ source("./UKBB_200KWES_CVD/GENESIS_adaptation_source.R")
 args=(commandArgs(TRUE))
 trait=as.character(args[1])
 gdsfile=as.character(args[2])
-phenfile=as.character(args[3])
-ID_col=as.character(args[4])
-nullfile=as.character(args[5])
-outfile=as.character(args[6])
+groupfile=as.character(args[3])
+phenfile=as.character(args[4])
+ID_col=as.character(args[5])
+nullfile=as.character(args[6])
+outfile=as.character(args[7])
 
 perform_burden_collapse(gdsfile=gdsfile,
                        groupfile=groupfile,
