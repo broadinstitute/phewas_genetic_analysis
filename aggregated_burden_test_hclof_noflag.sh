cfa66be3b28202c61ef16038b1f5c6fa2b9688f2
seuchoi <seuchoi@gmail.com>
2022-03-29 12:04:26 -0400

    update the sript

--- aggregated_burden_test_hclof_noflag.sh
+++ aggregated_burden_test_hclof_noflag.sh
@@ -5,6 +5,29 @@ set -o errexit
 # March 2022
 
 
-#sudo git clone https://github.com/broadinstitute/phewas_genetic_analysis.git
+# sudo git clone https://github.com/broadinstitute/phewas_genetic_analysis.git
 
-R CMD BATCH "--args ${trait} ${gdsfile} ${groupfile} ${phenfile} ${ID_col} ${nullfile} ${outfile}" phewas_genetic_analysis/src/aggregated_burden_test.R ${logfile}
+cd /
+pwd > ${logfile}
+ls > ${outfile}
+ls /phewas_genetic_analysis/src/aggregated_burden_test.R >> ${logfile}
+echo ${trait} >> ${logfile}
+#ls ${trait} >> ${logfile}
+echo ${gdsfile} >> ${logfile}
+ls  ${gdsfile} >> ${logfile}
+echo ${groupfile} >> ${logfile}
+ls  ${groupfile} >> ${logfile}
+echo ${phenfile} >> ${logfile}
+ls  ${phenfile} >> ${logfile}
+echo ${ID_col} >> ${logfile}
+#ls  ${ID_col} >> ${logfile}
+echo ${nullfile} >> ${logfile}
+ls  ${nullfile} >> ${logfile}
+echo ${outfile} >> ${logfile}
+ls  ${outfile} >> ${logfile}
+
+R CMD BATCH "--args ${trait} ${gdsfile} ${groupfile} ${phenfile} ${ID_col} ${nullfile} ${outfile}" /phewas_genetic_analysis/src/aggregated_burden_test.R ${logfile}
+
+# +
+#mv tmp/text.out ${logfile}
+#mv tmp/text2.out ${outfile}
