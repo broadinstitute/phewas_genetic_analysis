a6ad990a74551a2343b967e4717f9da9a1e60593
Seung Hoan Choi <seuchoi@gmail.com>
2023-07-14 13:08:35 -0400

    try new thing

---
+++ src/kernel_variance_component.R
@@ -0,0 +1,27 @@
+args=(commandArgs(TRUE))
+gdsfile=as.character(args[1])
+groupfile=as.character(args[2])
+phenfile=as.character(args[3])
+ID_col=as.character(args[4])
+nullfile=as.character(args[5])
+outfile=as.character(args[6])
+
+cat('\nReading in packages for analysis...\n')
+source("UKBB_200KWES_CVD/GENESIS_adaptation_source.R")
+source("TOPMed_AFib_pipeline/DNANexus/kernell_variance_component_modfied.R")
+source("TOPMed_AFib_pipeline/DNANexus/ExtractKernelStatistics_error_fixed.R")
+
+kernell_variance_component_v2(gdsfile=gdsfile,
+groupfile=groupfile,
+phenfile=phenfile,
+ID_col=ID_col,
+nullfile=nullfile,
+outfile=outfile,
+test="ExtractKernelStatistics",
+vc.test="Score",
+AF.max=0.001,
+MAC.max=Inf,
+use.weights=FALSE)
+
+sessionInfo()
+quit("no")
