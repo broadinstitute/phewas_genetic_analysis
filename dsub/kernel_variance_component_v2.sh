e0129a016ab57234b7ec3d90310a1cd43d60a651
Seung Hoan Choi <seuchoi@gmail.com>
2023-07-14 13:14:36 -0400

    update

--- dsub/kernel_variance_component_v2.sh
+++ dsub/kernel_variance_component_v2.sh
@@ -10,8 +10,9 @@ echo "phenotypefile is compressed"
 gunzip -c ${phenfile} > phenotypefile.tsv
 phenfile=phenotypefile.tsv
 fi
+ls /phewas_genetic_analysis/src/kernel_variance_component.R
 
-R CMD BATCH "--args ${chr22_gdsfile} ${chr22_groupfile} ${phenfile} ${ID_col} ${nullfile} ${chr22_outfile}" /TOPMed_AFib_pipeline/DNANexus/kernell_variance_component_aou.R ${chr22_logfile}
+R CMD BATCH "--args ${chr22_gdsfile} ${chr22_groupfile} ${phenfile} ${ID_col} ${nullfile} ${chr22_outfile}" /phewas_genetic_analysis/src/kernel_variance_component.R ${chr22_logfile}
 echo "chr22 ran"
 
 # run the analysis
