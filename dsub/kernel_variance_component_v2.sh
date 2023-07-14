71ab490ef15a81fd5f2dbf0cb4ef3f15ab5ca8c0
Seung Hoan Choi <seuchoi@gmail.com>
2023-07-14 14:28:56 -0400

    fix

--- dsub/kernel_variance_component_v2.sh
+++ dsub/kernel_variance_component_v2.sh
@@ -13,7 +13,8 @@ phenfile=phenotypefile.tsv
 fi
 ls /phewas_genetic_analysis/src/kernel_variance_component.R
 cat /phewas_genetic_analysis/src/kernel_variance_component.R
-R CMD BATCH "--args ${chr22_gdsfile} ${chr22_groupfile} ${phenfile} ${ID_col} ${nullfile} ${chr22_outfile}" /phewas_genetic_analysis/src/kernel_variance_component.R ${chr22_logfile}
+#R CMD BATCH "--args ${chr22_gdsfile} ${chr22_groupfile} ${phenfile} ${ID_col} ${nullfile} ${chr22_outfile}" /phewas_genetic_analysis/src/kernel_variance_component.R ${chr22_logfile}
+R CMD BATCH "--args ${chr22_gdsfile} ${chr22_groupfile} ${phenfile} ${ID_col} ${nullfile} ${chr22_outfile} ${test_type} ${af_cutoff} " /TOPMed_AFib_pipeline/DNANexus/kernell_variance_component_test.R ${chr22_logfile}
 echo "chr22 ran"
 
 # run the analysis
